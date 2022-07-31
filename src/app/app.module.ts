89e67eeef6bd62fca8d06d7a7935d9e56d739d6a
Ash Chambers <ashchambers304@gmail.com>
2022-07-31 14:42:33 +0100

    Refactored navigation to work with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CarouselComponent } from './components/shared/carousel/carousel.component';
-import { LandingComponent } from './components/sections/landing/landing.component';
-import { AboutComponent } from './components/sections/about/about.component';
-import { ProjectsComponent } from './components/sections/projects/projects.component';
-import { ServicesComponent } from './components/sections/services/services.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 import { accordionComponent } from './components/shared/accordion/accordion.component';
-import { TestimonialsComponent } from './components/sections/testimonials/testimonials.component';
-import { ContactComponent } from './components/sections/contact/contact.component';
+import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 import { TestimonialComponent } from './components/shared/testimonial/testimonial.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'testimonials', component: TestimonialComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarouselComponent,
-    LandingComponent,
     AboutComponent,
     ProjectsComponent,
-    ServicesComponent,
     accordionComponent,
     TestimonialsComponent,
     ContactComponent,
     TestimonialComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
