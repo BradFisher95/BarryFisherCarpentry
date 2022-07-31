dec71cac1eb926cc87721c06753f1b636f87d4af
Ash Chambers <ashchambers304@gmail.com>
2022-07-31 17:43:30 +0100

    Made new testimony page. Made footer. Fixed header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CarouselComponent } from './components/shared/carousel/carousel.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 import { HomeComponent } from './components/pages/home/home.component';
-import { AboutComponent } from './components/pages/about/about.component';
 import { ProjectsComponent } from './components/pages/projects/projects.component';
-import { accordionComponent } from './components/shared/accordion/accordion.component';
+import { AboutComponent } from './components/pages/about/about.component';
 import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
-import { TestimonialComponent } from './components/shared/testimonial/testimonial.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'testimonials', component: TestimonialComponent },
-  { path: 'contact', component: ContactComponent },
-];
+import { CarouselComponent } from './components/shared/carousel/carousel.component';
+import { accordionComponent } from './components/shared/accordion/accordion.component';
+import { TestimonialComponent } from './components/shared/testimonial/testimonial.component';
+import { NavLinksComponent } from './components/shared/nav-links/nav-links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
     CarouselComponent,
     AboutComponent,
     ProjectsComponent,
@@ -35,13 +30,10 @@ const routes: Routes = [
     TestimonialsComponent,
     ContactComponent,
     TestimonialComponent,
+    NavLinksComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule,
-    RouterModule.forRoot(routes),
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
