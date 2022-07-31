082c010b728153b58fc66c83d01766965ded62aa
Ash Chambers <ashchambers304@gmail.com>
2022-07-31 14:46:52 +0100

    re organised header and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
