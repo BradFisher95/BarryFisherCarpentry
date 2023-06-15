0e973d0727655afbaa0f56bd56539bb81a57b3cf
Brad Fisher <bfisher@DESKTOP-104B407.localdomain>
2023-06-15 14:11:22 +0100

    Added logic for netlify forms

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { NetlifyFormsService } from 'src/services/netlify-forms.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
   contactFormGroup = new FormGroup({
     name: new FormControl(''),
     telephone: new FormControl(''),
@@ -15,11 +17,28 @@ export class ContactComponent implements OnInit {
     enquiryDetails: new FormControl(''),
   });
 
-  constructor() {}
+  constructor(
+    private formService: NetlifyFormsService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  errorMsg = '';
 
   onSubmit(): void {
+    if (this.contactFormGroup.valid) {
+      const payload = this.contactFormGroup.value;
+      this.formService.submitEnquiry(payload).subscribe(
+        () => {
+          this.contactFormGroup.reset();
+          this.router.navigateByUrl('/contact-success');
+        },
+        (err) => {
+          this.errorMsg = err;
+        }
+      );
+    } else {
+      this.contactFormGroup.markAllAsTouched();
+    }
     console.log('Form Submitted!');
   }
 }
---
+++ src/services/netlify-forms.service.ts
@@ -0,0 +1,48 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NetlifyFormsService {
+  constructor(private http: HttpClient) {}
+
+  public submitEnquiry(enquiry: any): Observable<any> {
+    const entry = new HttpParams({
+      fromObject: {
+        'form-name': 'contactFormGroup',
+        ...enquiry,
+      },
+    });
+
+    return this.submitEntry(entry);
+  }
+
+  private submitEntry(entry: HttpParams): Observable<any> {
+    return this.http
+      .post('/', entry.toString(), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        responseType: 'text',
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(err: HttpErrorResponse): any {
+    let errMsg = '';
+
+    if (err.error instanceof ErrorEvent) {
+      errMsg = `A client-side error occurred: ${err.error.message}`;
+    } else {
+      errMsg = `A server-side error occurred. Code: ${err.status}. Message: ${err.message}`;
+    }
+
+    return throwError(errMsg);
+  }
+}
