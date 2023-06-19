1049570e072a27a94574976c569db0a923bfb468
Ash Chambers <ashchambers304@gmail.com>
2023-06-19 16:07:07 +0100

    scroll resets on router event. adjusted styling of headings and testimonials. adjusted scrollbar styling. changed email address on footer and contact page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,16 @@
-import {
-  Component,
-  ViewChildren,
-  ElementRef,
-  AfterViewInit,
-  QueryList,
-} from '@angular/core';
-import { NavigationService } from 'src/services/navigation.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChildren('section', { read: ElementRef })
-  Sections!: QueryList<ElementRef>;
-
-  constructor(private navService: NavigationService) {}
-
-  ngAfterViewInit(): void {
-    this.navService.Sections = this.Sections;
+export class AppComponent {
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      window.scrollTo(0, 0);
+    });
   }
 
   title = 'bfishercarpentry';
