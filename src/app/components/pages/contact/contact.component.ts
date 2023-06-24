fb04cc1798093264ec60203cf91f7e206d9534ef
Brad Fisher <bfisher@DESKTOP-104B407.localdomain>
2023-06-24 11:18:43 +0100

    updated contact form with full validation

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NetlifyFormsService } from 'src/services/netlify-forms.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -10,36 +9,64 @@ import { Router } from '@angular/router';
 })
 export class ContactComponent {
   contactFormGroup = new FormGroup({
-    name: new FormControl(''),
-    telephone: new FormControl('', null),
-    email: new FormControl(''),
-    postcode: new FormControl(''),
-    enquiryDetails: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    telephone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      ),
+    ]),
+    postcode: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/gm
+      ),
+    ]),
+    enquiryDetails: new FormControl('', [
+      Validators.required,
+      Validators.minLength(50),
+    ]),
   });
 
-  formSuccess = true;
+  get name() {
+    return this.contactFormGroup.get('name');
+  }
+  get telephone() {
+    return this.contactFormGroup.get('telephone');
+  }
+  get email() {
+    return this.contactFormGroup.get('email');
+  }
+  get postcode() {
+    return this.contactFormGroup.get('postcode');
+  }
+  get enquiryDetails() {
+    return this.contactFormGroup.get('enquiryDetails');
+  }
 
-  constructor(
-    private formService: NetlifyFormsService,
-    private router: Router
-  ) {}
+  formSuccess = false;
+  formFaliure = false;
+  formSubmissionAttempt = false;
 
-  errorMsg = '';
+  constructor(private formService: NetlifyFormsService) {}
 
   onSubmit(): void {
     console.log('form submission was callsed');
     if (this.contactFormGroup.valid) {
-      console.log('isValid');
       const payload = this.contactFormGroup.value;
       this.formService.submitEnquiry(payload).subscribe(
         () => {
           this.contactFormGroup.reset();
           this.formSuccess = true;
-          console.log(this.formSuccess);
+          this.formSubmissionAttempt = true;
         },
         (err) => {
-          this.errorMsg = err;
-          console.log(this.errorMsg);
+          this.formFaliure = true;
+          this.formSubmissionAttempt = true;
         }
       );
     } else {
